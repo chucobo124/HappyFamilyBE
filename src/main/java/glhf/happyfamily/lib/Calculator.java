baea0c669d036f18abcabb8dd6675b42587eb0bc
Lucas Chen <lucas.chen.formal@gmail.com>
2023-11-12 20:10:53 +0900

    Change to array list and related test case

--- src/main/java/glhf/happyfamily/lib/Calculator.java
+++ src/main/java/glhf/happyfamily/lib/Calculator.java
@@ -1,30 +1,47 @@
 package glhf.happyfamily.lib;
 
+import java.util.ArrayList;
+import javax.management.RuntimeErrorException;
 import glhf.happyfamily.models.Balance;
 
 public class Calculator {
-    private Balance[] balances;
+    private ArrayList<Balance> balances;
     private int total;
 
-    Calculator(Balance[] balances, int total) throws ExceptionInInitializerError {
+    Calculator(ArrayList<Balance> balances, int total) throws ExceptionInInitializerError {
         this.balances = balances;
         this.total = total;
+    }
+
+    public Balance[] calculate() throws RuntimeErrorException {
+        if (!canCalculate(total)) {
+            throw new RuntimeException("can't pass the validation");
+        }
+        return null;
+    }
+
+    public void appendBalance(Balance balance) {
+        this.balances.add(balance);
+    }
+
+    private Boolean canCalculate(int total) throws IllegalArgumentException {
         int balanceTotal = 0;
         for (Balance balance : balances) {
             balanceTotal += balance.getPaid();
         }
         if (balanceTotal != this.total) {
-            throw new ExceptionInInitializerError(
+            throw new IllegalArgumentException(
                     String.format("the total paid isn't match to input total, total paid:%d, input total:%d",
                             balanceTotal, total));
         }
+        return true;
     }
 
-    public Balance[] getBalances() {
+    public ArrayList<Balance> getBalances() {
         return this.balances;
     }
 
-    public void setBalance(Balance[] balances) {
+    public void setBalance(ArrayList<Balance> balances) {
         this.balances = balances;
     }
 
--- src/test/java/glhf/happyfamily/lib/CalculatorTest.java
+++ src/test/java/glhf/happyfamily/lib/CalculatorTest.java
@@ -3,31 +3,48 @@ package glhf.happyfamily.lib;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import glhf.happyfamily.models.Balance;
 
 public class CalculatorTest {
     final String accountingActivityID = "c3sd";
-    Balance[] balances = {
-            new Balance().setUserID("user1").setPaid(100).setAccountingActivityID(accountingActivityID),
-            new Balance().setUserID("user2").setPaid(50).setAccountingActivityID(accountingActivityID),
-            new Balance().setUserID("user3").setAccountingActivityID(accountingActivityID),
-            new Balance().setUserID("user4").setAccountingActivityID(accountingActivityID),
-    };
+    private ArrayList<Balance> balances = new ArrayList<Balance>();
+
+    @BeforeEach
+    void SetUp() {
+        balances = new ArrayList<Balance>();
+        balances.add(new Balance().setUserID("user1").setPaid(100).setAccountingActivityID(accountingActivityID));
+        balances.add(new Balance().setUserID("user2").setPaid(50).setAccountingActivityID(accountingActivityID));
+        balances.add(new Balance().setUserID("user3").setAccountingActivityID(accountingActivityID));
+        balances.add(new Balance().setUserID("user4").setAccountingActivityID(accountingActivityID));
+    }
 
     @Test
-    void InitCalculatorObjectShouldPass() {
+    void TestInitCalculatorObjectShouldPass() {
         assertDoesNotThrow(() -> {
             new Calculator(balances, 150);
         });
     };
 
     @Test
-    void InitCalculatorObjectShouldFailedWhenTotalIsNotMatched() {
-        ExceptionInInitializerError exception = assertThrows(
-                ExceptionInInitializerError.class,
+    void TestAppendBalance() {
+        Calculator calculator = new Calculator(balances, 150);
+        assertEquals(4, balances.size(), "assert the origin balance");
+        calculator.appendBalance(new Balance().setUserID("user5").setAccountingActivityID(accountingActivityID));
+        assertEquals(5, balances.size(), "the balance append should plus one");
+    }
+
+    @Test
+    void TestCalculateFunctionShouldFailedWhenTotalIsNotMatched() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
                 () -> {
-                    new Calculator(balances, 50);
+                    new Calculator(balances, 50).calculate();
                 });
         assertEquals("the total paid isn't match to input total, total paid:150, input total:50",
                 exception.getMessage());
