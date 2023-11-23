0cb5031f9aeb5bd5f283c82e7698c1c27c10b66d
Lucas Chen <lucas.chen.formal@gmail.com>
2023-11-24 01:11:10 +0900

    update calculator logic

--- src/main/java/glhf/happyfamily/lib/Calculator.java
+++ src/main/java/glhf/happyfamily/lib/Calculator.java
@@ -1,9 +1,20 @@
 package glhf.happyfamily.lib;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 import javax.management.RuntimeErrorException;
-import glhf.happyfamily.models.Balance;
 
+import glhf.happyfamily.models.Balance;
+import glhf.happyfamily.models.Lent;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class Calculator {
     private ArrayList<Balance> balances;
     private int total;
@@ -13,18 +24,79 @@ public class Calculator {
         this.total = total;
     }
 
-    public Balance[] calculate() throws RuntimeErrorException {
+    public Balance[] calculateEqually() throws RuntimeErrorException {
         if (!canCalculate(total)) {
             throw new RuntimeException("can't pass the validation");
         }
-        return null;
+        this.balances.sort((x, y) -> Integer.compare(x.getPaid(), y.getPaid()));
+        Stack<Object[]> paymentStack = new Stack<Object[]>();
+        Map<String, Set<Lent>> lentList = new HashMap<>();
+        Integer equalPaid = total / this.balances.size();
+        this.balances.forEach((x) -> {
+            Integer diff = equalPaid - x.getPaid();
+            // when diff is negative, meaning the user lent the money to someone and add
+            // to lent list
+            if (diff > 0) {
+                Set<Lent> currentLent = new HashSet<Lent>();
+                currentLent.add(Lent.builder().lent(diff).build());
+                x.setLents(currentLent);
+                lentList.put(x.getUserID(), x.getLents());
+                return;
+            }
+            // gathering the paid list by user
+            Object[] paymentUnit = new Object[2];
+            paymentUnit[0] = x.getUserID();
+            paymentUnit[1] = Math.abs(diff);
+            paymentStack.add(paymentUnit);
+        });
+        // get lent list
+        Object[] payer = paymentStack.pop();
+        for (Map.Entry<String, Set<Lent>> x : lentList.entrySet()) {
+            String userID = x.getKey();
+            Set<Lent> lent = x.getValue();
+            Integer totalLent = 0;
+            for (Lent l : lent) {
+                totalLent += l.getLent();
+            }
+            Set<Lent> currentUserLentList = new HashSet<Lent>();
+            while (totalLent > 0) {
+                String payerUserID = payer[0].toString();
+                Integer payerAmount = (Integer) payer[1];
+                Integer diff = payerAmount - totalLent;
+                Integer currentUserLent = totalLent;
+                if (diff >= 0) {
+                    payer[1] = diff;
+                }
+                if (diff < 0) {
+                    currentUserLent = payerAmount;
+                    payer = paymentStack.pop();
+                }
+                currentUserLentList.add(Lent.builder().lent(currentUserLent).toUserID(payerUserID).build());
+                lentList.put(userID, currentUserLentList);
+                totalLent -= currentUserLent;
+            }
+        }
+        // convert to balances
+        lentList.forEach((userID, lent) -> {
+            this.balances.forEach((b) -> {
+                if (b.getUserID() == userID) {
+                    b.setLents(lent);
+                }
+            });
+        });
+
+        Balance[] result = new Balance[this.balances.size()];
+        result = this.balances.toArray(result);
+
+        return result;
     }
 
     public void appendBalance(Balance balance) {
         this.balances.add(balance);
     }
 
-    private Boolean canCalculate(int total) throws IllegalArgumentException {
+    // Validate the total to see if total payment can equal to input total
+    public Boolean canCalculate(int total) throws IllegalArgumentException {
         int balanceTotal = 0;
         for (Balance balance : balances) {
             balanceTotal += balance.getPaid();
@@ -36,21 +108,4 @@ public class Calculator {
         }
         return true;
     }
-
-    public ArrayList<Balance> getBalances() {
-        return this.balances;
-    }
-
-    public void setBalance(ArrayList<Balance> balances) {
-        this.balances = balances;
-    }
-
-    public int getTotal() {
-        return this.total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
 }
--- src/test/java/glhf/happyfamily/lib/CalculatorTest.java
+++ src/test/java/glhf/happyfamily/lib/CalculatorTest.java
@@ -3,39 +3,38 @@ package glhf.happyfamily.lib;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.util.ArrayList;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import glhf.happyfamily.models.Balance;
+import glhf.happyfamily.models.Lent;
 
 public class CalculatorTest {
     final String accountingActivityID = "c3sd";
     private ArrayList<Balance> balances = new ArrayList<Balance>();
+    private Integer expectedTotal = 150;
 
     @BeforeEach
     void SetUp() {
         balances = new ArrayList<Balance>();
-        balances.add(new Balance().setUserID("user1").setPaid(100).setAccountingActivityID(accountingActivityID));
-        balances.add(new Balance().setUserID("user2").setPaid(50).setAccountingActivityID(accountingActivityID));
-        balances.add(new Balance().setUserID("user3").setAccountingActivityID(accountingActivityID));
-        balances.add(new Balance().setUserID("user4").setAccountingActivityID(accountingActivityID));
+        balances.add(Balance.builder().userID("user1").paid(100).accountingActivityID(accountingActivityID).build());
+        balances.add(Balance.builder().userID("user2").paid(50).accountingActivityID(accountingActivityID).build());
+        balances.add(Balance.builder().userID("user3").accountingActivityID(accountingActivityID).build());
+        balances.add(Balance.builder().userID("user4").accountingActivityID(accountingActivityID).build());
     }
 
     @Test
     void TestInitCalculatorObjectShouldPass() {
         assertDoesNotThrow(() -> {
-            new Calculator(balances, 150);
+            new Calculator(balances, expectedTotal);
         });
     };
 
     @Test
     void TestAppendBalance() {
-        Calculator calculator = new Calculator(balances, 150);
+        Calculator calculator = new Calculator(balances, expectedTotal);
         assertEquals(4, balances.size(), "assert the origin balance");
-        calculator.appendBalance(new Balance().setUserID("user5").setAccountingActivityID(accountingActivityID));
+        calculator.appendBalance(Balance.builder().userID("user5").accountingActivityID(accountingActivityID).build());
         assertEquals(5, balances.size(), "the balance append should plus one");
     }
 
@@ -44,9 +43,59 @@ public class CalculatorTest {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> {
-                    new Calculator(balances, 50).calculate();
+                    new Calculator(balances, 50).calculateEqually();
                 });
         assertEquals("the total paid isn't match to input total, total paid:150, input total:50",
                 exception.getMessage());
+    }
+
+    @Test
+    void TestCalculatorShouldReturnsTheEquallyResult() {
+        Calculator calculator = new Calculator(balances, expectedTotal);
+        Balance[] balances = calculator.calculateEqually();
+        assertEquals(balances.length, 4);
+        // Total paid 150, $37.5 (trim to 37) for each when separate equally
+        for (Balance b : balances) {
+            switch (b.getUserID()) {
+                case "user1":
+                    assertEquals(null, b.getLents(),
+                            "the first user paid the bill, shouldn't lent to anyone");
+                    break;
+                case "user2":
+                    assertEquals(null, b.getLents(),
+                            "the second user paid the bill, shouldn't lent to anyone");
+                    break;
+                case "user3":
+                    // the first user had borrow 63 to others
+                    // the second user had borrow 13 to others
+                    Lent actualLent = (Lent) b.getLents().toArray()[0];
+                    assertEquals(
+                            actualLent.getToUserID(), "user1",
+                            "lent to first user, since first user paid the hedgiest amount");
+                    assertEquals(
+                            actualLent.getLent(), 37,
+                            "lent to first user, since first user paid the hedgiest amount");
+                    break;
+                case "user4":
+                    // the first user had borrow 26 to others
+                    // the second user had borrow 13 to others
+                    Lent actualLent1 = (Lent) b.getLents().toArray()[0];
+                    Lent actualLent2 = (Lent) b.getLents().toArray()[1];
+                    assertEquals(
+                            "user1",
+                            actualLent2.getToUserID(),
+                            "lent to first user, since the balance is separate between both");
+                    assertEquals(
+                            actualLent2.getLent(), 26,
+                            "lent to second user, since the balance is separate between both");
+                    assertEquals(
+                            actualLent1.getToUserID(), "user2",
+                            "lent to first user, since the balance is separate between both");
+                    assertEquals(
+                            actualLent1.getLent(), 11,
+                            "lent to second user, since the balance is separate between both");
+                    break;
+            }
+        }
     }
 }
