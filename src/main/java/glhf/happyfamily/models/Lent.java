b26186d3cef39423dd4691e71678236ce21fb597
Lucas Chen <lucas.chen.formal@gmail.com>
2023-11-24 01:11:27 +0900

    add getter and setter decorator

--- src/main/java/glhf/happyfamily/models/Lent.java
+++ src/main/java/glhf/happyfamily/models/Lent.java
@@ -1,28 +1,21 @@
 package glhf.happyfamily.models;
 
-import java.util.Set;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Builder
-@Getter
 @Setter
-public class Balance {
+@Getter
+public class Lent {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String uuid;
-    private String accountingActivityID;
-    private String title;
-    private String description;
-    private int paid;
-    @OneToMany
-    private Set<Lent> lents;
-    private String userID;
+    private Integer lent;
+    private String toUserID;
 }
